eda4b01885ca38f1b70e48e35d605fa439dfb161
Bernard <bernarddonkor154@gmail.com>
2023-09-07 08:38:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - A c program that prints line of the standard error
  *
- * Return: 1 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
